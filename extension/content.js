d9c70e1f8c9260289364bc9787465d95c949824c
Jack Huang <Jack7100@gmail.com>
2016-01-08 17:20:03 -0500

    content.js added and modified manifest to include content scripts

---
+++ extension/content.js
@@ -0,0 +1,6 @@
+// $('div').hide()
+// $('body').append('<button id="show" type="button">Click to show content</button>')
+// $('#show').on('click', function(event){
+//   event.preventDefault();
+//   $('div').show();
+// })
