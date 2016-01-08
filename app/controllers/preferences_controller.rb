e554e3690453bbad2e78c8575a2551140a45cfbb
Arcangelo Petretta <arcpetretta@gmail.com>
2016-01-08 16:34:06 -0500

    Add destroy and edit routes to preferences controller

--- app/controllers/preferences_controller.rb
+++ app/controllers/preferences_controller.rb
@@ -3,10 +3,28 @@ class PreferencesController < ApplicationController
   def create
     mediaobj = Media.find(params[:preference][:media_id])
     preference = Preference.new(user: current_user, media: mediaobj)
+    if preference.save
+      redirect_to user_path(current_user.id)
+    else
+      @medias = Media.where(params[:preference][:media_id])
+      flash[:notice] = "Unable to add filter"
+      render 'media/index'
+    end
+  end
+
+  def update
+    preference = Preference.find(params[:id])
+      if preference.active == true
+        preference.update_attribute(:active, !true)
+        redirect_to user_path(current_user.id)
+      else
+        preference.update_attribute(:active, true)
+        redirect_to user_path(current_user.id)
+      end
   end
 
   def destroy
-    preference = Preference.find_by(id: params[:preference_id])
+    preference = Preference.find_by(id: params[:id])
     preference.destroy
     redirect_to user_path(current_user.id)
   end
