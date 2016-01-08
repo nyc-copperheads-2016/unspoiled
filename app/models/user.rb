36542e692c574c88d2c0fb51f36f18500596200d
Ryan Lesson <email": null>
2016-01-08 11:06:07 -0500

    Remove validation because has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,6 @@ class User < ActiveRecord::Base
   validates_uniqueness_of :username, :email
   validates :username, length: { minimum: 4 }
   validates :password, length: { minimum: 6 }
-  validates :password, confirmation: true
   validates_presence_of :password_confirmation
   validates :email, :format => { :with => /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 end
