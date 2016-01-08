08afd2412e65ef0f34404392cd88c75133ff3bf1
Ryan Lesson <email": null>
2016-01-08 12:26:25 -0500

    Add user dummy view files for testing

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+describe UsersController do
+
+  describe 'Get #new' do
+
+    it "assigns a new User to @user" do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it "renders the :new template" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+  end
+
+  describe 'POST #create' do
+    context "with valid attributes" do
+      it "saves the new user in the database"
+      it "redirects to root_path"
+    end
+  end
+
+  describe 'Get #show' do
+    it "assigns the requested user to @user"
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,9 +14,7 @@ RSpec.describe User, type: :model do
       it {should validate_length_of(:password)}
       it { should validate_presence_of(:username) }
       it { should validate_presence_of(:password) }
-       it { should validate_presence_of(:email) }
-
-
+      it { should validate_presence_of(:email) }
     end
   end
 end
