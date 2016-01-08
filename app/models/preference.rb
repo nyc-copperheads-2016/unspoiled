fd47ddc4dcbbb27a2ef605ae61c2ac78657cc0a7
Arcangelo Petretta <arcpetretta@gmail.com>
2016-01-08 17:10:25 -0500

    Add on_or_off methods for preferences...Used in user show page.

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -3,4 +3,13 @@ class Preference < ActiveRecord::Base
   belongs_to :media
 
   validates :user, :media, :active, presence: true
+
+
+  def on_or_off
+    if self.active == true
+      "on"
+    else
+      "off"
+    end
+  end
 end
