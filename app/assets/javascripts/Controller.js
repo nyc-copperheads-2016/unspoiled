a81f1b6e57bd2b01022e153722a7933b9615cc6a
Ryan Lesson <email": null>
2016-01-09 17:43:33 -0500

    Add initial movie search function

--- app/assets/javascripts/Controller.js
+++ app/assets/javascripts/Controller.js
@@ -1,1 +1,15 @@
-Controller.js
+function Controller(view) {
+  this.view = view;
+  view.controller = this;
+}
+
+
+
+
+
+
+// Controller.prototype.index = function() {
+//   Media.loadMedia().then(function(media){
+//     this.view.showFiftyTweets(media);
+//   }.bind(this));
+// }
--- app/assets/javascripts/Model.js
+++ app/assets/javascripts/Model.js
@@ -1,1 +1,49 @@
-Model.js
+var movieSearch = function(tv){
+   var tvUrl = 'http://api.themoviedb.org/3/',
+        mode = 'search/movie?query=',
+        input = tv,
+        key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+
+    return $.get(tvUrl+mode+input+key).then(function(response){
+    console.log(response);
+    });
+  };
+
+
+
+
+
+
+// $(document).ready(function() {
+
+//   function Category(json) {
+//   Object.assign(this,json);
+//   this.createdAt = new Date(this.created_at);
+//   this.updatedAt = new Date(this.updated_at);
+//   }
+
+//   Category.prototype.tvSearch = function(tv){
+//    var tvUrl = 'http://api.themoviedb.org/3/',
+//         mode = 'search/tv?query=',
+//         input = "breaking-bad",
+//         key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+
+//     return $.get(tvUrl+mode+input+key).then(function(response){
+//     console.log("tv"+response);
+//     });
+//   };
+// });
+
+
+  // Media.prototype.tvSearch = function(tv){
+  //  var tvUrl = 'http://api.themoviedb.org/3/',
+  //       mode = 'search/tv?query=',
+  //       input = "breaking-bad",
+  //       key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+
+  // return $.get(tvUrl+mode+input+key).then(function(response){
+  //   console.log("tv"+response);
+  // });
+
+
+
