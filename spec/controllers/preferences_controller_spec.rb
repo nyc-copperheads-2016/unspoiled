2897a2615c28a4808b056f67be74ad443d8e1a56
Ryan Lesson <email": null>
2016-01-08 20:30:03 -0500

    Add preference tests

--- spec/controllers/preferences_controller_spec.rb
+++ spec/controllers/preferences_controller_spec.rb
@@ -1,1 +1,29 @@
 require 'rails_helper'
+
+describe PreferencesController do
+
+  describe 'Post #create' do
+    context "with valid attributes" do
+      it "saves a new preference in the database" do
+        user1 = User.create(username: "bobby", email: "bobby@bobby.com", password: "123456789")
+        category1 = Category.create(category_type: "TV Show")
+        media1 = Media.create(title: "Game of Thrones", category: category1)
+        preference1 = Preference.create(user: user1, media: media1)
+        expect{post :create, preference: preference1, media: media1, user: user1}
+      end
+
+      it "redirects to user_path" do
+        user1 = User.create(username: "bobby", email: "bobby@bobby.com", password: "123456789")
+        category1 = Category.create(category_type: "TV Show")
+        media1 = Media.create(title: "Game of Thrones", category: category1)
+        preference1 = Preference.create(user: user1, media: media1)
+        post :create, preference: {preference1: {media_id: media1}}, user_id: user1.id
+        expect(response).to redirect_to user_path(user)
+      end
+    end
+  end
+
+end
+
+mediaobj = Media.find(params[:preference][:media_id])
+{prefe
