4d3806c7dfb8e458d72244c128daceca88a90310
Eliza Dunaway <email": null>
2016-01-09 12:38:51 -0500

    Change media_controller_spec.rb to rid merge conflicts

--- spec/controllers/media_controller_spec.rb
+++ spec/controllers/media_controller_spec.rb
@@ -3,14 +3,21 @@ require 'rails_helper'
 describe MediaController do
 
   describe 'GET #index' do
-
-    it "populates an array of all media objects that belongs to a certain category" do
       category1 = Category.create(category_type: "Movie")
       media1 = FactoryGirl.create(:media, title: "Smith", category_id: category1.id)
       media2 = FactoryGirl.create(:media, title: "Jones", category_id: category1.id)
+      get :index, category_id: category1.id
+      expect(assigns(:medias)).to match_array([media1, media2])
 
+    it "populates an array of media objects that belongs to a certain category" do
       get :index, category_id: category1.id
       expect(assigns(:medias)).to match_array([media1, media2])
     end
+
+    it "renders the :index template" do
+      get :index, category_id: category1.id
+      expect(response).to render_template :index
+    end
+
   end
 end
