eec846f81253761288acfcd41869ce6dff29afc9
Ryan Lesson <email": null>
2016-01-09 20:19:35 -0500

    Add title to returned object

--- app/assets/javascripts/Model.js
+++ app/assets/javascripts/Model.js
@@ -5,7 +5,8 @@ var movieSearch = function(movie){
       key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
     return $.get(url + mode + input + key).then(function(response){
-    movieCredits(response.results[0].id);
+
+    movieCredits(response.results[0].id,response.results[0].original_title);
     });
   };
 
@@ -16,39 +17,38 @@ var tvSearch = function(tv){
      key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
  return $.get(url + mode + input + key).then(function(response){
-    console.log(response)
-    tvCredits(response.results[0].id);
+    tvCredits(response.results[0].id,response.results[0].name);
   })
  }
 
 
-var movieCredits = function(id){
+var movieCredits = function(id,title){
  var url = 'http://api.themoviedb.org/3/',
      mode = 'movie/' +id+ '/credits?query=',
      input = "hello",
      key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
   return $.get(url + mode + key).then(function(response){
-    var cast = []
+    var cast = [title]
     response.cast.forEach(function(element){
       cast.push(element.character, element.name)
     });
-    console.log(cast)
+    return(cast)
   })
  }
 
 
-var tvCredits = function(id){
+var tvCredits = function(id,title){
   var url = 'http://api.themoviedb.org/3/',
       mode = "tv/" + id + "/credits?query=",
       key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
   return $.get(url + mode + key).then(function(response){
-    var cast = []
+    var cast = [title]
     response.cast.forEach(function(element){
       cast.push(element.character, element.name)
     });
-    console.log(cast)
+    return(cast)
   })
 }
 
