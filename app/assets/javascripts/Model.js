bcea2613f0eb3d11ab35592778ef8ff92e1bec06
Ryan Lesson <email": null>
2016-01-09 19:05:21 -0500

    Add function to retrive credits

--- app/assets/javascripts/Model.js
+++ app/assets/javascripts/Model.js
@@ -1,49 +1,55 @@
-var movieSearch = function(tv){
-   var tvUrl = 'http://api.themoviedb.org/3/',
-        mode = 'search/movie?query=',
-        input = tv,
-        key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+var movieSearch = function(movie){
+  var url = 'http://api.themoviedb.org/3/',
+      mode = 'search/movie?query=',
+      input = movie,
+      key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
-    return $.get(tvUrl+mode+input+key).then(function(response){
-    console.log(response);
+    return $.get(url + mode + input + key).then(function(response){
+    movieCredits(response.results[0].id);
     });
   };
 
+var tvSearch = function(tv){
+ var url = 'http://api.themoviedb.org/3/',
+     mode = 'search/tv?query=',
+     input = tv,
+     key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+
+ return $.get(url + mode + input + key).then(function(response){
+    console.log(response)
+    tvCredits(response.results[0].id);
+  })
+ }
 
 
+var movieCredits = function(id){
+ var url = 'http://api.themoviedb.org/3/',
+     mode = 'movie/' +id+ '/credits?query=',
+     input = "hello",
+     key = '&api_key=e0c6c7bded5055b146501304684b8f94';
+
+  return $.get(url + mode + key).then(function(response){
+    var cast = []
+    response.cast.forEach(function(element){
+      cast.push(element.character, element.name)
+    });
+    console.log(cast)
+  })
+ }
 
 
+var tvCredits = function(id){
+  var url = 'http://api.themoviedb.org/3/',
+      mode = "tv/" + id + "/credits?query=",
+      key = '&api_key=e0c6c7bded5055b146501304684b8f94';
 
-// $(document).ready(function() {
-
-//   function Category(json) {
-//   Object.assign(this,json);
-//   this.createdAt = new Date(this.created_at);
-//   this.updatedAt = new Date(this.updated_at);
-//   }
-
-//   Category.prototype.tvSearch = function(tv){
-//    var tvUrl = 'http://api.themoviedb.org/3/',
-//         mode = 'search/tv?query=',
-//         input = "breaking-bad",
-//         key = '&api_key=e0c6c7bded5055b146501304684b8f94';
-
-//     return $.get(tvUrl+mode+input+key).then(function(response){
-//     console.log("tv"+response);
-//     });
-//   };
-// });
-
-
-  // Media.prototype.tvSearch = function(tv){
-  //  var tvUrl = 'http://api.themoviedb.org/3/',
-  //       mode = 'search/tv?query=',
-  //       input = "breaking-bad",
-  //       key = '&api_key=e0c6c7bded5055b146501304684b8f94';
-
-  // return $.get(tvUrl+mode+input+key).then(function(response){
-  //   console.log("tv"+response);
-  // });
-
+  return $.get(url + mode + key).then(function(response){
+    var cast = []
+    response.cast.forEach(function(element){
+      cast.push(element.character, element.name)
+    });
+    console.log(cast)
+  })
+}
 
 
