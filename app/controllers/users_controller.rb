5582a84e9446ed5b86b253c221fa97cebd64c178
Ryan Lesson <email": null>
2016-01-10 21:24:17 -0500

    Add error messages to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,9 +5,9 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.new(user_params)
-    if user.save
-      session[:user_id] = user.id
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
       render :new
