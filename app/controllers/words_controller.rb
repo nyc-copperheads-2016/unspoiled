dc0422271b296b8fd6a9c875936db1ce0e79774d
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-13 16:53:34 -0500

    downcase word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -7,7 +7,7 @@ class WordsController < ApplicationController
     media = Media.find_by(id: params[:media_id])
     new_preference = media.preferences.create!(user: current_user)
     new_word.each do |word|
-      Word.create!(word: word, preference: new_preference)
+      Word.create!(word: word.downcase, preference: new_preference)
     end
     redirect_to user_path(current_user.id)
   end
