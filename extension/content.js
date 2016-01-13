f20e581ceb4d2b3be004ce4f779a377e84fea45b
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-13 16:34:07 -0500

    Remove old code

--- extension/content.js
+++ extension/content.js
@@ -46,28 +46,6 @@ chrome.storage.local.get("filter", function(obj) {
   }
 })
 
-// function findMatch(text) {
-//   if (!text) {
-//     return;
-//   }
-//   // var match = false
-//   // if (array_of_words.indexOf(text.toLowerCase()) != -1) {
-//     // $.each(array_of_words, function(index, element){
-//       for (var i=0; i < array_of_words.length - 1; i++) {
-//       // debugger
-//         if (text.toLowerCase().match(array_of_words[i]) != null) {
-//       // if (text.toLowerCase().search(array_of_words[index]) != -1 ) {
-//       // match = true;
-//       // console.log("hi")
-//           console.log("check", text, array_of_words[i]);
-//           return true
-//         }
-//     // return true
-//     // match = true
-//     }
-//   return false
-// }
-
 
 var textNodes = $("*").contents().filter(function(){ return this.nodeType == 3; });
 function hideWord(tags) {
@@ -81,29 +59,7 @@ function hideWord(tags) {
     })
   });
 }
-//   $.each(tags, function(key, element) {
-//     console.log(element)
-//     // debugger
-//     if (findMatch(element.text)) {
-//       // debugger
-//       // var childNodes = element.childNodes
-//       // for (var i=0; i< childNodes.length - 1; i++) {
-//         // debugger
-//         // if (childNodes[i].innerHTML != undefined && findMatch(childNodes[i])) {
-//       // $.each(element, function(index, element) {
-//       // console.log($(element)[0])
-//       // $(element).hide()
-//       // var replaceImg = '<img src="http://localhost:3000/assets/icon-945908b8301759cca3dc7d98c417383df6e8697fc6362343b526a240b599fc94.png" alt="Unspoiled!" />'
-//       // console.log($(element))
-//       // debugger
-//       console.log('will hide', element);
 
-//       $(element).hide()
-//      $(element).parent().append('<a class= "meep" href="#" style="border: 3px solid red; width: 200px;">Unspoiled (click to show spoiler)</a>')
-//         // }
-//       // })
-//     }
-//   })
 
 $(document).ready(function() {
   loggedIn()
