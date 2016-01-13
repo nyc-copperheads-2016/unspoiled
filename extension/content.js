1b58be57edd9d6e70726e031e8af77e46be2b547
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-12 23:05:41 -0500

    hide words but need to fix filter function

--- extension/content.js
+++ extension/content.js
@@ -40,7 +40,7 @@ function setFilter() {
 }
 
 // var allTags = document.querySelectorAll('a, p, span, h1, h2, h3, h4, h5, h6, caption')
-var allTags = $("*").not("html").not("head").not("script").not('meta')
+var allTags = $("*").not("html").not("head, script, title, link").not('meta').not('title').not("body, div")
 // var array_of_words = ["netflix", "streaming", "jon snow"]
 var array_of_words = []
 chrome.storage.local.get("filter", function(obj) {
@@ -50,24 +50,40 @@ chrome.storage.local.get("filter", function(obj) {
 })
 
 function findMatch(text) {
-  var match = false
-  if (array_of_words.indexOf(text.toLowerCase()) != -1) {
-    match = true
-  }
-  return match
+  // var match = false
+  // if (array_of_words.indexOf(text.toLowerCase()) != -1) {
+    // $.each(array_of_words, function(index, element){
+      for (var i=0; i < array_of_words.length-1; i++) {
+      // debugger
+        if (text.toLowerCase().match(array_of_words[i]) != null) {
+      // if (text.toLowerCase().search(array_of_words[index]) != -1 ) {
+      // match = true;
+      // console.log("hi")
+          return true
+        }
+    // return true
+    // match = true
+    }
+  return false
 }
 
 function hideWord(tags) {
   $.each(tags, function(key, element) {
     if (findMatch($(element).html())) {
+    debugger
+      // $.each(element, function(index, element) {
       // console.log($(element)[0])
       // $(element).hide()
       // var replaceImg = '<img src="http://localhost:3000/assets/icon-945908b8301759cca3dc7d98c417383df6e8697fc6362343b526a240b599fc94.png" alt="Unspoiled!" />'
+      // console.log($(element))
+      // debugger
       $(element).hide()
       $(element).parent().append('<a class= "meep" href="#" style="border: 3px solid red; width: 200px;">Unspoiled (click to show spoiler)</a>')
+      // })
     }
   })
 }
+
 
 $(document).ready(function() {
   loggedIn()
