a4608c93a6146430fb791d71bb18c8879f57d2f9
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-13 18:44:07 -0500

    only letters can be put as filters, no underscores, special characters or integers

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -3,11 +3,15 @@ class WordsController < ApplicationController
   end
 
   def create
-    new_word_array = params[:word][:word].split(',')
+    new_word_array = params[:word][:word].split(/[\, ]/)
     media = Media.find_by(id: params[:media_id])
-    new_preference = media.preferences.create!(user: current_user)
-    new_word_array.each do |word|
-      Word.create!(word: word.downcase.strip, preference: new_preference)
+    new_preference = media.preferences.new(user: current_user)
+    if new_preference.save
+      new_word_array.each do |word|
+        if !word.match(/\W/) && !word.match(/\d/) && !word.match(/_/) && !word.blank?
+          Word.create!(word: word.downcase.strip, preference: new_preference)
+        end
+      end
     end
     redirect_to user_path(current_user.id)
   end
