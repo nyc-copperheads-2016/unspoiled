71aab00d4aacf914f15854b9d0db0e57f3366bdf
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-13 00:56:20 -0500

    Add initial code for adding buttons

--- app/assets/javascripts/Controller.js
+++ app/assets/javascripts/Controller.js
@@ -16,5 +16,18 @@
 //     // findTv("the-expanse")
 //     // findMovie(movieName)
 // })
+$(document).ready(function(){
 
+  $('#toggle_event_editing button').click(function(){
+    if($(this).hasClass('locked_active') || $(this).hasClass('unlocked_inactive')){
+      /* code to do when unlocking */
+    }else{
+      /* code to do when locking */
+    }
 
+    $('#toggle_event_editing button').eq(0).toggleClass('locked_inactive locked_active btn-default btn-info');
+    $('#toggle_event_editing button').eq(1).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
+  });
+})
+
+//button toggle.
