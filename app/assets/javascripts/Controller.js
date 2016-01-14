dc33bbd5303a5919fc13c881e1c2b46789069b4f
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-14 10:37:09 -0500

    remove localhost comments

--- app/assets/javascripts/Controller.js
+++ app/assets/javascripts/Controller.js
@@ -1,21 +1,3 @@
-// $(document).ready(function(){
-//   $('.filter').on('submit', function(event) {
-//     var movieName = $(event.target).serializeArray()[3].value.toLowerCase().split(' ').join('-')
-//     Movie.findMovie("top-gun").then(function(cast) {
-//       var movieObj = new Movie(cast.results[0].id, "top-gun")
-//       var getCast = movieObj.credits()
-//       getCast.then(function(){
-//         return movieObj.cast
-//         // debugger
-//     })
-//       .then(function() {
-//       $.post("http://localhost:3000/filter_words", movieObj)
-//       })
-//     })
-//   })
-//     // findTv("the-expanse")
-//     // findMovie(movieName)
-// })
 $(document).ready(function(){
 
   $('#toggle_event_editing button').click(function(){
@@ -29,5 +11,3 @@ $(document).ready(function(){
     $('#toggle_event_editing button').eq(1).toggleClass('unlocked_inactive unlocked_active btn-info btn-default');
   });
 })
-
-//button toggle.
