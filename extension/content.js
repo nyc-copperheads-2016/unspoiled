0332d8cc126e39a349bf08d626a632c91894bb2b
Arcangelo Petretta & Jack Huang <pair+arcpetretta.jhack32@devbootcamp.com>
2016-01-13 19:37:53 -0500

    remove commented console logs

--- extension/content.js
+++ extension/content.js
@@ -1,7 +1,7 @@
 var loggedOn = false;
 function loggedIn() {
-  // $.get("https://thawing-badlands-1060.herokuapp.com/user_logged_in", function(data) {
-    $.get("http://localhost:3000/user_logged_in", function(data) {
+  $.get("https://thawing-badlands-1060.herokuapp.com/user_logged_in", function(data) {
+    // $.get("http://localhost:3000/user_logged_in", function(data) {
     if (data) {
       sessionStorage.loggedIn = true
     }
@@ -18,12 +18,10 @@ function loggedIn() {
 }
 
 function setFilter() {
-  // $.get("https://thawing-badlands-1060.herokuapp.com/filtered_words", function(data) {
-  $.get("http://localhost:3000/filtered_words", function(data) {
+  $.get("https://thawing-badlands-1060.herokuapp.com/filtered_words", function(data) {
+  // $.get("http://localhost:3000/filtered_words", function(data) {
     chrome.storage.local.set({filter: data});
-    // console.log("Filter set", data);
     chrome.storage.local.get(function(obj) {
-      // console.log("callback: ", obj)
       if (sessionStorage.loggedIn === "true"){
         if (obj.unspoiledOn === true) {
           if (obj.filter.length > 1) {
@@ -52,7 +50,6 @@ function hideWord(tags) {
   textNodes.each(function(index, node) {
     array_of_words.forEach(function(word){
       if (node.wholeText.toLowerCase().match(word) != null) {
-        // console.log("word", word, node.parentNode)
         $(node.parentNode).hide()
         $(node.parentNode).parent().append('<a class= "meep" href="#" style="color: white; background-color: purple; border: 3px solid lightblue; width: 200px;">Unspoiled (click to show spoiler)</a>')
       }
